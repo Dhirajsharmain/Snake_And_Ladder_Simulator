ec2a7cf7a008146ba01e0f4f376b7cf9d3ff94b0
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-01 20:12:37 +0530

    Modified: Game started with posotion 0

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,1 +1,11 @@
 //Welcome to snake and ladder simulator
+
+public class SnakeAndLadder {
+     public static void main(String[] args) {
+       int playerPosition = 0;
+
+       //Player Position Start With Zero
+       System.out.println("Player Position:" + playerPosition);
+
+    }
+}
