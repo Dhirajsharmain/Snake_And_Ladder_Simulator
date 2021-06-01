82a88d5dfc5c5fce1f8270f8ba781eaafa3083f6
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-01 21:10:07 +0530

    Modified: Generated Random Number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,8 @@
 public class SnakeAndLadder {
      public static void main(String[] args) {
        int playerPosition = 0;
+       //Player Position Start With Zero
+       System.out.println("Player Position:" + playerPosition);
        
        //Roll dice to generate random number between 1 to 6
        int dice =(int) Math.floor(Math.random() * 10) % 6 + 1;
